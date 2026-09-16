ff195c953b62d2ae176b7523ad39a8ed70941e51
BeanieMen <jainarjav886@gmail.com>
2026-09-16 20:51:54 +0530

    feat(memory): page table and page table interrupts

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,35 @@
 #![no_main]
 #![feature(abi_x86_interrupt)]
 
+use bootloader::{BootInfo, entry_point};
+use x86_64::VirtAddr;
 use core::panic::PanicInfo;
 mod gdt;
 mod interrupts;
-mod mem;
+mod memory;
 mod vga_buffer;
+mod mem;
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
     init();
 
+    use x86_64::registers::control::Cr3;
+
+    let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let level_4_table = unsafe { memory::active_level_4_table(physical_memory_offset) };
+    println!("Level 4 page table at: {:p}", level_4_table);
+
+    for (i, entry) in level_4_table.iter().enumerate() {
+        if !entry.is_unused() {
+            println!("l4 entry {}: {:?}", i, entry);
+        }
+    }
+
     loop {
-        // crate::print!("-");
-        // x86_64::instructions::hlt();
+        x86_64::instructions::hlt();
     }
 }
 
